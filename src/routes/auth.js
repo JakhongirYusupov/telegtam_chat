a569cc64e82807d12c62a392673b375dea839d92
JakhongirYusupov <93903956+JakhongirYusupov@users.noreply.github.com>
2022-01-15 11:15:29 +0500

    routers added

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 const router = Router()
 
-router.get('/register', (req, res) => {
+router.post('/register', (req, res) => {
     res.send('register')
 })
 
 router.post('/login', (req, res) => {
     res.send('login')
 })
+
+export default router
--- src/server.js
+++ src/server.js
@@ -1,11 +1,9 @@
 import express from "express";
-// import PORT from process.env.PORT || 5000
-const PORT = process.env.PORT || 5000
+import { PORT } from '../config.js'
 const app = express()
 
-app.get('/', (req, res) => {
-    res.send('Hello')
-})
+import authRouter from './routes/auth.js'
+app.use('/auth', authRouter)
 
 
 app.listen(PORT, () => console.log('Server is running http://localhost:' + PORT))
